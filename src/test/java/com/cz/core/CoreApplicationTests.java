ca5b8270caeb22a9c56da7e2043d92596876855d
지금이야 <cz2123xc@gmail.com>
2022-05-05 16:36:32 +0900

    Initial commit

---
+++ src/test/java/com/cz/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
