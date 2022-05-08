62110dbb047a290b61ccf0ecb5b0e3b5c316ed3a
지금이야 <cz2123xc@gmail.com>
2022-05-09 04:03:21 +0900

    싱글톤 매커티즘 테스트중

--- src/main/java/com/cz/core/AppConfig.java
+++ src/main/java/com/cz/core/AppConfig.java
@@ -15,26 +15,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
-    public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
-//        return new FixDiscountPolicy();
-    }
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
 
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 
     @Bean
     public OrderService orderService() {
         return new OrderServiceImpl( memberRepository(), discountPolicy() );
     }
-
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+    }
 
 }
