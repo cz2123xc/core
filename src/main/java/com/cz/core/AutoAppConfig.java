00d43154dfe080e9e85468aca1c1391c30cdd804
지금이야 <cz2123xc@gmail.com>
2022-05-11 00:18:37 +0900

    컴포넌트에 exclude 와 수동 include 지정

--- src/main/java/com/cz/core/AutoAppConfig.java
+++ src/main/java/com/cz/core/AutoAppConfig.java
@@ -6,7 +6,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 // 컴포넌트 스캔은 자동으로 등록이다. 수동으로 등록하는 Configuration 은 자동 등록에서 제외한다. 실무에서는 딱히 제외하진 않음. 현재는 이전에 작성한 코드가 있어서 제외힘.
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class))
+@ComponentScan(
+//        basePackages = {"com.cz.core"}, // 스캔할 패키지
+//        basePackageClasses = {AutoAppConfig.class}, // 스캔할 패키지에 있는 클래스
+        // 지정하지 않으면 ComponentScan 의 package 를 기준으로 찾는다
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class
+        ))
 public class AutoAppConfig {
 
 
---
+++ src/test/java/com/cz/core/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.cz.core.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
